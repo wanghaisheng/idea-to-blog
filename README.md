fd6f9b61399d6b2086e9318fb70e5bf5c4f2b90c
HeisenBerg? <admin@tiktokastudio.com>
2024-04-25 03:24:08 +0800

    Initial commit

---
+++ README.md
@@ -0,0 +1,55 @@
+<div align="center">
+  <a href="https://github.com/filippofinke/web-to-pdf">
+    <img width="100px" src="https://github.com/filippofinke/web-to-pdf/assets/37296364/b0922e37-c624-47a2-8b9c-fe5db383485f">
+  </a>
+
+  <br />
+  <h3 align="center">web-to-pdf</h3>
+
+  <p align="center">
+    A web app that allows you to convert a website to a PDF file using Vercel Serverless Functions.
+    <br />
+    <a href="https://github.com/filippofinke/web-to-pdf"><strong>Explore the docs »</strong></a>
+    <br />
+    <a href="https://web-to-pdf-filippofinke.vercel.app/"><strong>Live demo »</strong></a>
+    <br />
+    <br />
+    <a href="https://github.com/filippofinke/web-to-pdf/issues">Report Bug</a>
+    ·
+    <a href="https://github.com/filippofinke/web-to-pdf/issues">Request Feature</a>
+  </p>
+</div>
+
+## Install
+
+```sh
+npm install
+```
+
+## Development
+
+```sh
+vercel dev
+```
+
+## Deployment
+
+```sh
+vercel deploy
+```
+
+## Author
+
+👤 **Filippo Finke**
+
+- Website: https://filippofinke.ch
+- Github: [@filippofinke](https://github.com/filippofinke)
+- LinkedIn: [@filippofinke](https://linkedin.com/in/filippofinke)
+
+## Show your support
+
+Give a ⭐️ if this project helped you!
+
+<a href="https://www.buymeacoffee.com/filippofinke">
+  <img src="https://github.com/filippofinke/filippofinke/raw/main/images/buymeacoffe.png" alt="Buy Me A McFlurry">
+</a>
---
+++ api/index.ts
@@ -0,0 +1,62 @@
+import { NextApiRequest, NextApiResponse } from "next";
+const playwright = require("playwright-aws-lambda");
+
+const getDomain = (url: string) => {
+  return new URL(url).hostname;
+};
+
+const validUrl = (url: string) => {
+  try {
+    new URL(url);
+    return true;
+  } catch (error) {
+    return false;
+  }
+};
+
+export default async function handler(
+  request: NextApiRequest,
+  response: NextApiResponse
+) {
+  // replace /api/
+  let url = request.url?.replace("/api/", "");
+
+  if (!url) {
+    response.status(400).send("Missing url parameter");
+    return;
+  }
+
+  if (!validUrl(url as string)) {
+    response.status(400).send("Invalid url parameter");
+    return;
+  }
+
+  try {
+    const browser = await playwright.launchChromium({
+      headless: true,
+    });
+    const context = await browser.newContext();
+
+    const page = await context.newPage();
+
+    console.log("url", url);
+    await page.goto(url as string);
+
+    const pdfBytes = await page.pdf({
+      format: "a4",
+    });
+    await browser.close();
+
+    let fileName = getDomain(url as string) + ".pdf";
+
+    response.setHeader("Content-Type", "application/pdf");
+    response.setHeader(
+      "Content-Disposition",
+      'inline; filename="' + fileName + '"'
+    );
+
+    response.status(200).send(pdfBytes);
+  } catch (error: any) {
+    response.status(500).json({ error: error.message });
+  }
+}
